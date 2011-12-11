c3d4081b67b12dece5422e5eadf97b31733d9908
Gussi <gussi@gussi.is>
2011-12-11 20:52:33 +0000

    Changes to SQL files

--- sql/whitelist_player.sql
+++ sql/whitelist_player.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS `whitelist_player` (
   `comment` varchar(1024) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Comment',
   `type` enum('whitelist','blacklist') COLLATE utf8_unicode_ci NOT NULL COMMENT 'Type',
   `expire` int(10) unsigned NOT NULL COMMENT 'Expire date',
-  PRIMARY KEY (`player`)
+  PRIMARY KEY (`player`,`type`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
