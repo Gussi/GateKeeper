0e172077202167aff1126c29154e11d8c39e88da
Gussi <gussi@gussi.is>
2012-01-08 15:15:40 +0000

    Shifted code out of Util to AbstractCommand class
    Also some refactoring of method names

---
+++ src/main/java/is/gussi/bukkit/plugin/whitelist/command/AbstractCommand.java
@@ -0,0 +1,131 @@
+package is.gussi.bukkit.plugin.whitelist.command;
+
+import is.gussi.bukkit.plugin.whitelist.Data;
+import is.gussi.bukkit.plugin.whitelist.data.DataCIDR;
+import is.gussi.bukkit.plugin.whitelist.data.DataPlayer;
+
+import java.util.regex.Pattern;
+
+public abstract class AbstractCommand {
+	public static enum CommandAction {
+		ADD, REMOVE, CHECK
+	}
+
+	/**
+	 * Get command action
+	 * 
+	 * @param action
+	 * @return CommandAction
+	 */
+	protected CommandAction parseAction(String action) {
+		if(action.equals("add") || action.equals("a")) {
+			return CommandAction.ADD;
+		}
+		if(action.equals("remove") || action.equals("rem") || action.equals("rm") || action.equals("r") || action.equals("d")) {
+			return CommandAction.REMOVE;
+		}
+		if(action.equals("check") || action.equals("info") || action.equals("c") || action.equals("i")) {
+			return CommandAction.CHECK;
+		}
+		return null;
+	}
+
+	/**
+	 * Get command data
+	 * 
+	 * @param data
+	 * @return Data
+	 */
+	protected Data parseData(String data) {
+		if(data.contains(".")) {
+			// Assuming it's an IP
+			if(!data.contains("/")) {
+				data = data + "/32";
+			}
+			// Validate
+			Pattern validate = Pattern.compile("((2[0-5]{2}|2[0-4]\\d|1\\d{2}|[1-9]\\d|\\d)\\.){3}(2[0-5]{2}|2[0-4]\\d|1\\d{2}|[1-9]\\d|\\d)(/(3[012]|[12]\\d|\\d))?");
+			if(!validate.matcher(data).matches()) {
+				return null;
+			}
+			return new DataCIDR(data);
+		} else {
+			// Assuming it's a player, validate
+			Pattern validate = Pattern.compile("[A-Za-z0-9_]{1,16}");
+			if(!validate.matcher(data).matches()) {
+				return null;
+			}
+			return new DataPlayer(data);
+		}
+	}
+	
+	/**
+	 * Convert something like 1Y2M3w4d5h6m7s to milliseconds
+	 *
+	 * @param time
+	 * @return
+	 * @throws Exception Invalid time format
+	 */
+	public long parseTime(String time) throws Exception {
+		long seconds = 0;
+		StringBuilder stack = new StringBuilder();
+		for(int i = 0; i < time.length(); ++i) {
+			if(Character.isDigit(time.charAt(i))) {
+				stack.append(time.charAt(i));
+			} else {
+				int s = Integer.parseInt(stack.toString());
+				stack = new StringBuilder();
+
+				// Seconds
+				if(time.charAt(i) == 's') {
+					seconds += s;
+					continue;
+				}
+
+				// Minutes
+				s = s * 60;
+				if(time.charAt(i) == 'm') {
+					seconds += s;
+					continue;
+				}
+
+				// Hours
+				s = s * 60;
+				if(time.charAt(i) == 'h') {
+					seconds += s;
+					continue;
+				}
+
+				// Days
+				s = s * 24;
+				if(time.charAt(i) == 'd') {
+					seconds += s;
+					continue;
+				}
+
+				// Weeks
+				s = s * 7;
+				if(time.charAt(i) == 'w') {
+					seconds += s;
+					continue;
+				}
+
+				// Months
+				s = s * 4;
+				if(time.charAt(i) == 'M') {
+					seconds += s;
+					continue;
+				}
+
+				// Years
+				s = s * 12;
+				if(time.charAt(i) == 'Y') {
+					seconds += s;
+					continue;
+				}
+
+				throw new Exception("Invalid time format");
+			}
+		}
+		return seconds*1000;
+	}
+}
--- src/main/java/is/gussi/bukkit/plugin/whitelist/command/CommandBlacklist.java
+++ src/main/java/is/gussi/bukkit/plugin/whitelist/command/CommandBlacklist.java
@@ -3,16 +3,13 @@ package is.gussi.bukkit.plugin.whitelist.command;
 import java.util.Date;
 
 import is.gussi.bukkit.plugin.whitelist.Data;
-import is.gussi.bukkit.plugin.whitelist.Util;
-import is.gussi.bukkit.plugin.whitelist.Util.CommandAction;
 import is.gussi.bukkit.plugin.whitelist.Whitelist;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-// TODO: Make abstract parent class for this and CommandWhitelist, with common methods
-public class CommandBlacklist implements CommandExecutor {
+public class CommandBlacklist extends AbstractCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender cs, Command command, String alias, String[] args) {
@@ -20,8 +17,8 @@ public class CommandBlacklist implements CommandExecutor {
 			return false;
 		}
 
-		CommandAction action = Util.getCommandAction(args[0]);
-		Data data = Util.getCommandData(args[1]);
+		CommandAction action = this.parseAction(args[0]);
+		Data data = this.parseData(args[1]);
 
 		if(action == null || data == null) {
 			return false;
@@ -29,7 +26,7 @@ public class CommandBlacklist implements CommandExecutor {
 
 		if(args.length > 2) {
 			try {
-				long time = Util.getTime(args[2]);
+				long time = this.parseTime(args[2]);
 				data.setExpire(new Date(time + new Date().getTime()));
 			} catch (Exception e1) {
 				e1.printStackTrace();
